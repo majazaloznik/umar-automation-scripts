f16c8252a58d44f256cfe3866df6d0481d075391
majazaloznik <maja.zaloznik@gmail.com>
2022-07-07 10:50:55 +0200

    Fixed typos in file names

--- bash-scripts/local-surs-changes-api-update.bat
+++ bash-scripts/local-surs-changes-api-update.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-"C:\Program Files\R\R-4.1.2\bin\x64\Rscript.exe" -e "source('\\\\192.168.38.7\\public$\\Avtomatizacija\\umar-automation-scripts\\R\\surs-chages-api-update.R')" > \\192.168.38.7\public$\Avtomatizacija\umar-automation-scripts\logs\surs-chages-api-update.Rout 2>&1
+"C:\Program Files\R\R-4.1.2\bin\x64\Rscript.exe" -e "source('\\\\192.168.38.7\\public$\\Avtomatizacija\\umar-automation-scripts\\R\\surs-changes-api-update.R')" > \\192.168.38.7\public$\Avtomatizacija\umar-automation-scripts\logs\surs-changes-api-update.Rout 2>&1
--- bash-scripts/surs-changes-api-update.bat
+++ bash-scripts/surs-changes-api-update.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-"C:\Program Files\R\R-4.1.3\bin\x64\Rscript.exe" -e "source('\\\\192.168.38.7\\public$\\Avtomatizacija\\umar-automation-scripts\\R\\surs-chages-api-update.R')" > \\192.168.38.7\public$\Avtomatizacija\umar-automation-scripts\logs\surs-chages-api-update.Rout 2>&1
+"C:\Program Files\R\R-4.1.3\bin\x64\Rscript.exe" -e "source('\\\\192.168.38.7\\public$\\Avtomatizacija\\umar-automation-scripts\\R\\surs-changes-api-update.R')" > \\192.168.38.7\public$\Avtomatizacija\umar-automation-scripts\logs\surs-changes-api-update.Rout 2>&1
 CLS
