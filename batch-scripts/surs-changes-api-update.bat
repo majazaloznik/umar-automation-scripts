541801c32d4a76f82644f2aacab585d0a6e5aa16
majazaloznik <maja.zaloznik@gmail.com>
2022-07-14 16:37:03 +0200

    Fix paths for git in batch script and stop R from downloading fucking dependencies all the bloody time.

--- batch-scripts/surs-changes-api-update.bat
+++ batch-scripts/surs-changes-api-update.bat
@@ -1,7 +1,8 @@
 @ECHO OFF
 Rem this is for running the surs-changes-api-update script and saving the outputs. And then commiting and pushing the changes to github.
 "C:\Program Files\R\R-4.1.3\bin\x64\Rscript.exe" -e "source('\\\\192.168.38.7\\public$\\Avtomatizacija\\umar-automation-scripts\\R\\surs-changes-api-update.R')" > \\192.168.38.7\public$\Avtomatizacija\umar-automation-scripts\logs\surs-changes-api-update.Rout 2>&1
-git add ../data/surs-changes-api-changes.rds ../data/surs-changes-api-current.rds ../data/surs-changes-api-tomorrow.rds ../logs/log.rds ../logs/log_surs-changes-api-update.rds ../logs/surs-changes-api-update.Rout
-git commit -m "Umar data bot :robot: - SURS change API update."
+pushd \\192.168.38.7\public$\Avtomatizacija\umar-automation-scripts\
+git add data\surs-changes-api-changes.rds data\surs-changes-api-current.rds data\surs-changes-api-tomorrow.rds logs\log.rds logs\log_surs-changes-api-update.rds logs\surs-changes-api-update.Rout
+git commit -m "Umar data bot :robot: - daily SURS change API update."
 git push
 CLS
