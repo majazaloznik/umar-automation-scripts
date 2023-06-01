b0e53cf9ce1629372587c9a33649da59f0333664
Maja Založnik <mzaloznik@umar.local>
2023-06-01 14:41:10 +0200

    added SURS update scripts

---
+++ R/surs-update-database.R
@@ -0,0 +1,39 @@
+#devtools::install_github("majazaloznik/SURSfetchR")
+library(dplyr)
+library(SURSfetchR)
+# logging in as  maintainer
+con <- DBI::dbConnect(RPostgres::Postgres(),
+                      dbname = "platform",
+                      host = "localhost",
+                      port = 5432,
+                      user = "majaz",
+                      password = Sys.getenv("PG_MZ_PSW"),
+                      client_encoding = "utf8")
+# set schema search path
+DBI::dbExecute(con, "set search_path to platform")
+
+# # check you're connected ok and see how many series are in the series table
+# tbl( con, "series") %>%
+#   summarise(n = n())
+
+# get list of all surs tables
+tbl(con, "table") %>%
+  filter(source_id  == 1) |>
+  select(code) |>
+  collect() -> df
+
+# update all SURS tables
+system.time(purrr::walk(df$code, ~insert_new_data(.x, con)))
+
+
+# debugonce(insert_new_data)
+# out <- insert_new_data("0300260S", con)
+
+# # update series selection list
+# df <- UMARaccessR::get_all_series_wtable_names(con)
+# UMARaccessR::create_selection_excel(df, outfile = "O:\\Users\\MZaloznik\\seznam_serij")
+
+# # update html report
+rmarkdown::render("M:/analysis/umar_master_report/docs/html_test.Rmd",
+                  output_file = "O:/Avtomatizacija/data-platform/indikatorji_test.html")
+
---
+++ batch-scripts/surs-update-database.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+Rem this is for running the surs-update-database.R script which updates all the surs series in the platform database.
+"C:\Program Files\R\R-4.1.3\bin\x64\Rscript.exe" -e "source('\\\\192.168.38.7\\public$\\Avtomatizacija\\umar-automation-scripts\\R\\surs-update-database.R')" > \\192.168.38.7\public$\Avtomatizacija\umar-automation-scripts\logs\surs-update-database.Rout 2>&1
+CLS
