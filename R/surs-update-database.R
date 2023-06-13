b297446c95fd5c6fa919a080288c71adf67129bb
majazaloznik <maja.zaloznik@gmail.com>
2023-06-13 14:09:20 +0200

    Update script to production

--- R/surs-update-database.R
+++ R/surs-update-database.R
@@ -16,22 +16,22 @@ DBI::dbExecute(con, "set search_path to platform")
 # # tbl( con, "series") %>%
 # #   summarise(n = n())
 #
-# # get list of all surs tables
-# tbl(con, "table") %>%
-#   filter(source_id  == 1) |>
-#   select(code) |>
-#   collect() -> df
+# get list of all surs tables
+tbl(con, "table") %>%
+  filter(source_id  == 1) |>
+  select(code) |>
+  collect() -> df
 #
-# # update all SURS tables
-# system.time(purrr::walk(df$code, ~insert_new_data(.x, con)))
-#
+# update all SURS tables
+system.time(purrr::walk(df$code, ~insert_new_data(.x, con)))
+
 #
 # # debugonce(insert_new_data)
 # # out <- insert_new_data("0300260S", con)
 #
 # # # update series selection list
-# # df <- UMARaccessR::get_all_series_wtable_names(con)
-# # UMARaccessR::create_selection_excel(df, outfile = "O:\\Users\\MZaloznik\\seznam_serij")
+# df <- UMARaccessR::get_all_series_wtable_names(con)
+# UMARaccessR::create_selection_excel(df, outfile = "O:\\Avtomatizacija\\data-platform\\seznam_serij")
 
 # # update html report
 rmarkdown::render("\\\\192.168.38.7\\user_home\\mzaloznik\\analysis\\umar_master_report\\docs\\html_test.Rmd",
