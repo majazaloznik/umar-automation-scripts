088eacef26406262eab9acd2b66155bbb35acdf7
majazaloznik <maja.zaloznik@gmail.com>
2023-06-20 14:26:33 +0200

    Fix report compilation to avoid file lock

--- R/surs-update-database.R
+++ R/surs-update-database.R
@@ -36,7 +36,25 @@ system.time(purrr::walk(df$code, ~insert_new_data(.x, con)))
 # df <- UMARaccessR::get_all_series_wtable_names(con)
 # UMARaccessR::create_selection_excel(df, outfile = "O:\\Avtomatizacija\\data-platform\\seznam_serij")
 
+# report outputs
+time <- format(Sys.time(), "%d.%b%Y_%H%m")
+outfile <- paste0("\\\\192.168.38.7\\public$\\Avtomatizacija\\data-platform\\indikatorji_test",
+                  time, ".html")
+origfile <- paste0("\\\\192.168.38.7\\public$\\Avtomatizacija\\data-platform\\indikatorji_test.html")
+
 # # update html report
 rmarkdown::render("\\\\192.168.38.7\\user_home\\mzaloznik\\analysis\\umar_master_report\\docs\\html_test.Rmd",
-                  output_file = "\\\\192.168.38.7\\public$\\Avtomatizacija\\data-platform\\indikatorji_test.html")
+                  output_file = outfile)
+
+# in case someone has the report open in a preview pane, this should prevent the code from failing.
+tryCatch({
+  # Try to rename the file
+  file.rename(outfile, origfile)
+}, warning = function(w) {
+  # Handle warnings here
+  print(paste("Warning: ", w))
+}, error = function(e) {
+  # Handle errors here
+  print(paste("Error: ", e))
+})
 
--- R/zrsz-update-database.R
+++ R/zrsz-update-database.R
@@ -15,7 +15,25 @@ DBI::dbExecute(con, paste("set search_path to", schema))
 # update BO table
 zrsz_bo_script(con, schema)
 
+
+# report outputs
+time <- format(Sys.time(), "%d.%b%Y_%H%m")
+outfile <- paste0("\\\\192.168.38.7\\public$\\Avtomatizacija\\data-platform\\indikatorji_test",
+                  time, ".html")
+origfile <- paste0("\\\\192.168.38.7\\public$\\Avtomatizacija\\data-platform\\indikatorji_test.html")
+
 # # update html report
 rmarkdown::render("\\\\192.168.38.7\\user_home\\mzaloznik\\analysis\\umar_master_report\\docs\\html_test.Rmd",
-                  output_file = "\\\\192.168.38.7\\public$\\Avtomatizacija\\data-platform\\indikatorji_test.html")
+                  output_file = outfile)
 
+# in case someone has the report open in a preview pane, this should prevent the code from failing.
+tryCatch({
+  # Try to rename the file
+  file.rename(outfile, origfile)
+}, warning = function(w) {
+  # Handle warnings here
+  print(paste("Warning: ", w))
+}, error = function(e) {
+  # Handle errors here
+  print(paste("Error: ", e))
+})
