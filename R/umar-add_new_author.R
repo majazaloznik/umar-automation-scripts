e6e26cdd046670fcd8b133e6f7a84350a1ebd723
majazaloznik <maja.zaloznik@gmail.com>
2023-08-24 11:39:18 +0200

    Add add_new_author script.

---
+++ R/umar-add_new_author.R
@@ -0,0 +1,28 @@
+# package
+# devtools::install_github("majazaloznik/UMARfetchR", dependencies = FALSE)
+library(UMARfetchR)
+
+# setup
+con <- DBI::dbConnect(RPostgres::Postgres(),
+                      dbname = "platform",
+                      host = "localhost",
+                      port = 5432,
+                      user = "majaz",
+                      password = Sys.getenv("PG_MZ_PSW"),
+                      client_encoding = "utf8")
+DBI::dbExecute(con, "set search_path to platform")
+
+data_location <- "O:/Avtomatizacija/umar-data"
+
+# new author info
+name = "Matevž Hribernik"
+initials = "MH"
+email = "matevz.hribernik@gov.si"
+schema = "platform"
+
+# add new author
+add_new_author(name,
+               initials = initials,
+               email = email,
+               con = con,schema = schema,
+               data_location = data_location)
