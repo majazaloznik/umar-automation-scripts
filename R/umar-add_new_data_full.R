d87d09b50e21ae19aedc342587c8181f9e88acd3
majazaloznik <maja.zaloznik@gmail.com>
2023-08-31 22:30:28 +0200

    Add full data script and batch file

---
+++ R/umar-add_new_data_full.R
@@ -0,0 +1,54 @@
+################################################################################
+
+# setup packages and connections
+home <- "\\\\192.168.38.7\\public$\\Avtomatizacija\\furs-surs-soap\\"
+setwd(home)
+Sys.setenv(http_proxy="http://proxy.gov.si:80")
+Sys.setenv(http_proxy_user="http://proxy.gov.si:80")
+Sys.setenv(https_proxy="http://proxy.gov.si:80")
+Sys.setenv(https_proxy_user="http://proxy.gov.si:80")
+
+# devtools::install_github("majazaloznik/UMARfetchR")
+# devtools::install_github("majazaloznik/UMARaccessR", dependencies = FALSE,  INSTALL_opts=c("--no-multiarch"))
+
+
+library(gmailr)
+options(gargle_oauth_email = TRUE)
+gm_auth_configure(path ="data/credentials-umar.json")
+gm_auth(email = TRUE, cache = ".secret")
+
+# package
+library(UMARfetchR)
+library(UMARaccessR)
+# database connection
+con <- DBI::dbConnect(RPostgres::Postgres(),
+                      dbname = "platform",
+                      host = "localhost",
+                      port = 5432,
+                      user = "postgres",
+                      password = Sys.getenv("PG_PG_PSW"),
+                      client_encoding = "utf8")
+DBI::dbExecute(con, "set search_path to platform")
+schema <- "platform"
+
+dir_path <- "\\\\192.168.38.7\\public$\\Avtomatizacija\\umar-data"
+log_path <- "\\\\192.168.38.7\\public$\\Avtomatizacija\\umar-automation-scripts\\logs\\UMARfetchR\\"
+
+# run over all metadata files
+meta_filenames <- list.files(path = dir_path, pattern = "^umar_serije_metadata_",
+                    recursive = TRUE, full.names = TRUE)
+data_filenames <- list.files(path = dir_path, pattern = "^umar_serije_podatki_",
+                             recursive = TRUE, full.names = TRUE)
+
+for (i in seq(length(meta_filenames))){
+  update_data(meta_filenames[i], data_filenames[i], con, schema,
+                           path = log_path)
+}
+
+#
+# # run over single data file
+# initials <- "MH"
+# meta_filename <- paste0(dir_path, "\\", initials, "\\umar_serije_metadata_", initials, ".xlsx")
+# data_filename <- paste0(dir_path, "\\", initials,"\\umar_serije_podatki_", initials, ".xlsx")
+# update_data(meta_filename, data_filename, con, schema, path = log_path)
+#
---
+++ batch-scripts/umar-add_new_data.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+Rem this is for running the umar-add_new_data_full.R script for all the data files and imports any new stuff into the database.
+"C:\Program Files\R\R-4.1.3\bin\x64\Rscript.exe"  -e "source('\\\\192.168.38.7\\public$\\Avtomatizacija\\umar-automation-scripts\\R\\umar-add_new_data_full.R')" > \\192.168.38.7\public$\Avtomatizacija\umar-automation-scripts\logs\umar-add_new_data_full.Rout 2>&1
+pushd \\192.168.38.7\public$\Avtomatizacija\umar-automation-scripts\
+git config --global http.proxy http://proxy.gov.si:80
+git add  logs\umar-add_new_data_full.Rout
+git commit -m "Umar data bot :robot: - automatic UMAR data update."
+git push
+PAUSE
